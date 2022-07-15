14193edb6a6c88f3fd2c8abdc759ba1fdc8d04dc
Emna ouni <emna.ouni@esprit.tn>
2022-07-15 16:31:34 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -5,15 +5,16 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var ReservationsRouter = require('./routes/Reservations');
-var promotionRouter = require('./routes/promotion');
+
 
 var app = express();
 //-------------------------------------------------------
 //-------------------------------------------------------
 //WS
-var reservation = require('./ws/ReservationWS');
-var promotion = require('./ws/promotionWS');
+var cat=require('./ws/categorieWS');
+var prodd=require('./ws/produitWS');
+var recc=require('./ws/reclamationWS');
+
 
 
 //-------------------------------------------------------
@@ -33,8 +34,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 //-------------------------------------------------------
 //ROUTER
 app.use('/', indexRouter);
-app.use('/reservation', reservation);
-app.use('/promotion', promotion);
+app.use('/categorie', cat);
+app.use('/produit', prodd);
+app.use('/reclamation', recc);
 
 
 
@@ -42,29 +44,29 @@ app.use('/promotion', promotion);
 //-------------------------------------------------------
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 var mysql = require("mysql");
 //Database connection
-app.use(function(req, res, next) {
-    res.locals.connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'premia_sports'
-    });
-    res.locals.connect();
-    next();
+app.use(function(req, res, next){
+	res.locals.connection = mysql.createConnection({
+		host     : 'localhost',
+		user     : 'root',
+		password : '',
+		database : 'premia_sports'
+	});
+	res.locals.connect();
+	next();
 });
 module.exports = app;
